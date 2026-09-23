746a512acc21d09a3a4d565f74a7866a3afc5cd6
vivekdev10 <vivekpayaal.dev@gmail.com>
2026-09-23 20:06:52 +0530

    i have just merged changes from feat/a to main

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ function add(a, b) {
   return a + b;
 }
 
+function sub(a, b) {
+  if (!(a > b)) throw new Error("A should be bigger than b");
+  return a - b;
+}
+
 add(5, 5);
 add(10, 5);
 add(100, 5);
 
+sub(5, 1);
